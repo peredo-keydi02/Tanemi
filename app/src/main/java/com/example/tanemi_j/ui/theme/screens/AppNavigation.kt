f4b98a40515d0c7eb6e6e816cffec5ea43ecb56d
VianeyArteaga <choisoobinmx@gmail.com>
2025-03-30 14:43:33 -0600

    Integracion del traductor

--- app/src/main/java/com/example/tanemi_j/ui/theme/screens/AppNavigation.kt
+++ app/src/main/java/com/example/tanemi_j/ui/theme/screens/AppNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.tanemi_j.ui.theme.screens
 
+import android.speech.tts.TextToSpeech
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -8,17 +9,18 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tanemi_j.ui.theme.auth.AuthViewModel
+import java.util.*
 
 @Composable
-fun AppNavigation(authViewModel: AuthViewModel) {
+fun AppNavigation(authViewModel: AuthViewModel, textToSpeech: TextToSpeech) {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = "login") {
         composable("login") { LoginScreen(navController, authViewModel) }
         composable("registro") { RegistroScreen(navController, authViewModel) }
         composable("menu") { MenuScreen(navController, authViewModel) }
-        composable("traductor") { TraductorScreen(navController, authViewModel) }
+        composable("traductor") { TraductorScreen(navController, authViewModel, textToSpeech) }
         composable("recuperarcontrasena") { RecuperarContrasena(navController, authViewModel) }
-        composable("modificarcontrasena") { ModificarContrasenaScreen(navController,authViewModel) }
+        composable("modificarcontrasena") { ModificarContrasenaScreen(navController, authViewModel) }
     }
 }
