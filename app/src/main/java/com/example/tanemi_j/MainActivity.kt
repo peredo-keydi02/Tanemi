6521bc265e21f310f1bf5fa946520164350d3b11
Keydi Anahi Peredo Mendoza <keydi3346@gmail.com>
2025-03-31 22:28:24 -0600

    eliminacion de datos basura

--- app/src/main/java/com/example/tanemi_j/MainActivity.kt
+++ app/src/main/java/com/example/tanemi_j/MainActivity.kt
@@ -13,7 +13,6 @@ import com.example.tanemi_j.ui.theme.auth.AuthViewModelFactory
 import com.example.tanemi_j.ui.theme.auth.UserRepository
 import com.example.tanemi_j.ui.theme.screens.AppNavigation
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.messaging.FirebaseMessaging
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -39,17 +38,6 @@ class MainActivity : ComponentActivity() {
             AppNavigation(authViewModel = authViewModel, textToSpeech = textToSpeech)
         }
 
-        // Obtener el token de FCM
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "Error al obtener token FCM", task.exception)
-                return@addOnCompleteListener
-            }
-            val token = task.result
-            Log.d(TAG, "Token FCM obtenido: $token")
-            // Aquí podrías enviar el token a la base de datos, o confiar en onNewToken() del servicio.
-            // Por ejemplo, podrías llamar a una función similar a sendTokenToServer(token)
-        }
     }
 
     override fun onDestroy() {
